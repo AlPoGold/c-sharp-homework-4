bd9e60c5ec341887b480981161e1b2d3e8f19a58
Polina Kolesnikova <polina@example.com>
2023-03-06 16:30:35 +0400

    adding 25 exercise

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Write("Please, enter 2 numbers with space between: number and power: ");
+string[] array = Console.ReadLine()!.Split();
+int number = int.Parse(array[0]);
+int power = int.Parse(array[1]);
+
+Console.WriteLine($"{number} in {power} power equal {Math.Pow(number, power)}");
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
